1a7653c6fcc0405f3bf2b74000c1d4bfbbe58284
VuTruongPhuoc <vuphuoc7722@gmail.com>
2024-08-08 12:18:34 +0700

    Update Header

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -12,13 +12,15 @@ import {
     faSignOut,
 } from '@fortawesome/free-solid-svg-icons';
 import Tippy from '@tippyjs/react';
+import { Link } from 'react-router-dom';
+import 'tippy.js/dist/tippy.css';
 
+import routesConfig from '~/config/routes';
 import Button from '~/components/Button';
 import styles from './Header.module.scss';
 import images from '~/assets/images';
 import Menu from '~/components/Popper/Menu';
 import Search from '~/components/Layout/components/Search';
-import 'tippy.js/dist/tippy.css';
 import { InboxIcon, MessagesIcon, UploadIcon } from '~/components/Icons';
 import Image from '~/components/Image';
 
@@ -98,7 +100,9 @@ function Header() {
             <header className={cx('wrapper')}>
                 <div className={cx('inner')}>
                     <div className={cx('logo')}>
-                        <img src={images.logo.default} alt="Tiktok" />
+                        <Link to={routesConfig.home}>
+                            <img src={images.logo.default} alt="Tiktok" />
+                        </Link>
                     </div>
 
                     <Search />
--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -53,45 +53,60 @@ function Search() {
         setShowResult(false);
     };
 
+    const handleSpaceChange = (e) => {
+        const searchValue = e.target.value;
+        if (!searchValue.startsWith(' ')) {
+            setSearchValue(searchValue);
+        }
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
     return (
         <>
-            <HeadlessTippy
-                interactive={true}
-                visible={showResult && searchResult.length > 0}
-                render={(attrs) => (
-                    <div className={cx('search-result')} tabIndex="-1" {...attrs}>
-                        <PopperWrapper>
-                            <h3 className={cx('search-title')}>Accounts</h3>
-                            {searchResult.map((result) => (
-                                <AccountItem key={result.id} data={result} onClick={() => setShowResult(false)} />
-                            ))}
-                        </PopperWrapper>
-                    </div>
-                )}
-                onClickOutside={handleHideResult}
-            >
-                <div className={cx('search')}>
-                    <input
-                        ref={inputRef}
-                        value={searchValue}
-                        placeholder="Search"
-                        spellCheck={false}
-                        onChange={(e) => setSearchValue(e.target.value.trimStart())}
-                        onFocus={() => setShowResult(true)}
-                    />
-                    {!!searchValue && !loading && (
-                        <button className={cx('clear')} onClick={handleClear}>
-                            <FontAwesomeIcon icon={faCircleXmark} />
-                        </button>
+            {/* Using a wrapper <div> or tag around the reference element solves this 
+            by creating a new parentNode context.  */}
+            <div>
+                <HeadlessTippy
+                    interactive={true}
+                    visible={showResult && searchResult.length > 0}
+                    render={(attrs) => (
+                        <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                            <PopperWrapper>
+                                <h3 className={cx('search-title')}>Accounts</h3>
+                                {searchResult.map((result) => (
+                                    <AccountItem key={result.id} data={result} onClick={() => setShowResult(false)} />
+                                ))}
+                            </PopperWrapper>
+                        </div>
                     )}
+                    onClickOutside={handleHideResult}
+                >
+                    <div className={cx('search')}>
+                        <input
+                            ref={inputRef}
+                            value={searchValue}
+                            placeholder="Search"
+                            spellCheck={false}
+                            onChange={handleSpaceChange}
+                            onFocus={() => setShowResult(true)}
+                        />
+                        {!!searchValue && !loading && (
+                            <button className={cx('clear')} onClick={handleClear}>
+                                <FontAwesomeIcon icon={faCircleXmark} />
+                            </button>
+                        )}
 
-                    {loading && <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />}
+                        {loading && <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />}
 
-                    <button className={cx('search-btn')}>
-                        <SearchIcon />
-                    </button>
-                </div>
-            </HeadlessTippy>
+                        <button className={cx('search-btn')} onMouseDown={handleSubmit}>
+                            <SearchIcon />
+                        </button>
+                    </div>
+                </HeadlessTippy>
+            </div>
         </>
     );
 }
