394eea31adcf31029ef46eb7aeeb9a036d3767b5
VuTruongPhuoc <vuphuoc7722@gmail.com>
2024-12-28 00:11:20 +0700

    update following

--- src/pages/Following/index.js
+++ src/pages/Following/index.js
@@ -4,6 +4,7 @@ function Following() {
             <h1>Following page</h1>
             <h2>This is following page</h2>
             <h3>This is following page check conflict</h3>
+            <h4>This is main</h4>
         </>
     );
 }
